96f596dfb882530148370cb6d33c9af095c3fde6
Andresantos86 <86andresantos@gmail.com>
2023-10-31 06:17:53 -0300

    add database.js and .env

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,9 +1,5 @@
-const Http = require('http')
 
 test("GET TO /api/v1/status should return 200", async ()=>{
-  //const response = await fetch("http://localhost:3000/api/vi/status")
-  Http.get("http://localhost:3000/api/v1/status",res => {
-    expect(res.status).toBe(200)
-    });
-  
+  const response = await fetch("http://localhost:3000/api/v1/status")  
+  expect(response.status).toBe(200)
 })
