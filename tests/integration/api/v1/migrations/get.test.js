2f23729eff9c63a0046bf1ea56371c477c862e93
Andresantos86 <86andresantos@gmail.com>
2025-04-13 08:54:16 -0300

    build: dependencies

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,7 +8,6 @@ beforeAll(async () => {
 
 test("GET TO /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
-  console.log("resposta", response);
   expect(response.status).toBe(200);
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
